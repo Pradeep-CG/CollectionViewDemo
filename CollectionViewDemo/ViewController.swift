adc16030780dd7bd8790866277d9ab7d24058702
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-06-06 19:48:54 +0530

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by Pradeep on 06/06/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
